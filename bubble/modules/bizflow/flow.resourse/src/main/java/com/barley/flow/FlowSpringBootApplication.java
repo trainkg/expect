5efe092b5b0f78bc8cf1ac873db8c7e49eab67fb
triankg <peculiar.1@163.com>
2020-04-12 21:22:09 +0800

    Update pom.xml

--- bubble/modules/bizflow/flow.resourse/src/main/java/com/barley/flow/FlowSpringBootApplication.java
+++ bubble/modules/bizflow/flow.resourse/src/main/java/com/barley/flow/FlowSpringBootApplication.java
@@ -1,14 +1,23 @@
 package com.barley.flow;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * Spring Boot 入口
  * 
  * @author peculiar.1@163.com
  * @version $ID: FlowSpringBootApplication.java, V1.0.0 2020年4月12日 上午10:07:08 $
  */
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 public class FlowSpringBootApplication {
     
     public static void main(String[] args) {
-        
+        SpringApplication.run(FlowSpringBootApplication.class, args);
     }
 }
