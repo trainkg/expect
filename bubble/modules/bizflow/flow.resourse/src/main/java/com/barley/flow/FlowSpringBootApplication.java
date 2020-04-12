80408aa5251590e909a3df42d40dc8452453a8a7
triankg <peculiar.1@163.com>
2020-04-12 10:33:43 +0800

    JDK 版本更新为1.8

---
+++ bubble/modules/bizflow/flow.resourse/src/main/java/com/barley/flow/FlowSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.barley.flow;
+
+/**
+ * Spring Boot 入口
+ * 
+ * @author peculiar.1@163.com
+ * @version $ID: FlowSpringBootApplication.java, V1.0.0 2020年4月12日 上午10:07:08 $
+ */
+public class FlowSpringBootApplication {
+    
+    public static void main(String[] args) {
+        
+    }
+}
