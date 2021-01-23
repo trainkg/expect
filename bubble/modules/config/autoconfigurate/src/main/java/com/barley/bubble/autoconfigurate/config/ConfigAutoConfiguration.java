943684a69a93e867c948f768641e4b7568e7935e
triankg <peculiar.1@163.com>
2021-01-23 10:48:09 +0800

    updated

--- bubble/modules/config/autoconfigurate/src/main/java/com/barley/bubble/autoconfigurate/config/ConfigAutoConfiguration.java
+++ bubble/modules/config/autoconfigurate/src/main/java/com/barley/bubble/autoconfigurate/config/ConfigAutoConfiguration.java
@@ -1,20 +1,20 @@
 package com.barley.bubble.autoconfigurate.config;
 
 import org.springframework.beans.factory.annotation.Configurable;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.context.annotation.Bean;
-
-import com.barley.config.service.CodeTableBaseService;
-import com.barley.config.service.CodeTableServiceImpl;
 
 @Configurable
+/*
+ * @MapperScan(basePackages = {"com.barley.**.mappers"})
+ * 
+ * @ComponentScan(basePackages = { "com.barley.config" })
+ */
 public class ConfigAutoConfiguration {
 
-	@Bean
-	@ConditionalOnMissingBean(value = CodeTableBaseService.class)
-	public CodeTableBaseService configService() {
-		System.out.println("get default");
-		return new CodeTableServiceImpl();
-	}
+//	@Bean
+//	@ConditionalOnMissingBean(value = CodeTableBaseService.class)
+//	public CodeTableBaseService configService() {
+//		System.out.println("get default");
+//		return new CodeTableServiceImpl();
+//	}
 
 }
