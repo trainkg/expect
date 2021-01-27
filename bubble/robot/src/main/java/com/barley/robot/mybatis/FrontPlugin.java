a13c63d39fddcb8fa2cb6ff41565f002daacd20f
triankg <peculiar.1@163.com>
2021-01-27 21:45:39 +0800

    update

--- bubble/robot/src/main/java/com/barley/robot/mybatis/FrontPlugin.java
+++ bubble/robot/src/main/java/com/barley/robot/mybatis/FrontPlugin.java
@@ -22,7 +22,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class FrontPlugin extends org.mybatis.generator.api.PluginAdapter {
 
-	public static List<AbstractFrontGenerator> generators = new ArrayList<AbstractFrontGenerator>();
+	public static List<AbstractFrontGenerator<?>> generators = new ArrayList<AbstractFrontGenerator<?>>();
 
 	static {
 		generators.add(new FormGenerator());
